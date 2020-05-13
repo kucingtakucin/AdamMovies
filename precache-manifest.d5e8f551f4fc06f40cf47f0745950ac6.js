72fc8c9ee89d1b247c747fa872dcdb0fe52e5dda
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-05-13 20:12:29 +0700

    Updates

---
+++ precache-manifest.d5e8f551f4fc06f40cf47f0745950ac6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "627f7647a51d8d0570fb97dbc4c62de9",
+    "url": "/AdamMovies/index.html"
+  },
+  {
+    "revision": "09fda0c0e691c08a2cd9",
+    "url": "/AdamMovies/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "019bd50be0dc081113a6",
+    "url": "/AdamMovies/static/css/main.ce9a18af.chunk.css"
+  },
+  {
+    "revision": "09fda0c0e691c08a2cd9",
+    "url": "/AdamMovies/static/js/2.3d37fe97.chunk.js"
+  },
+  {
+    "revision": "150e7ebf1b16402b62fb665d1a7869df",
+    "url": "/AdamMovies/static/js/2.3d37fe97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "019bd50be0dc081113a6",
+    "url": "/AdamMovies/static/js/main.b159f246.chunk.js"
+  },
+  {
+    "revision": "82b5f9a1ca00213602d9",
+    "url": "/AdamMovies/static/js/runtime-main.1aa84c17.js"
+  },
+  {
+    "revision": "8b055f01b73ad5dc771b2428fdecc34e",
+    "url": "/AdamMovies/static/media/Cuprum-Regular-webfont.8b055f01.woff"
+  },
+  {
+    "revision": "74295694cbd3cf50532fb9f55622c038",
+    "url": "/AdamMovies/static/media/IndieFlower-webfont.74295694.woff"
+  }
+]);
