ca6ef60b84c55ecd3bfbfc91c889d64f50343fbd
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-05-13 20:53:15 +0700

    Updates

--- precache-manifest.d5e8f551f4fc06f40cf47f0745950ac6.js
+++ precache-manifest.d5e8f551f4fc06f40cf47f0745950ac6.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "929394cf98b08b8ebac205175f3a3ca1",
+    "revision": "627f7647a51d8d0570fb97dbc4c62de9",
     "url": "/AdamMovies/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/AdamMovies/static/css/2.de424728.chunk.css"
   },
   {
-    "revision": "e8bfd4120d598bf26ab9",
+    "revision": "019bd50be0dc081113a6",
     "url": "/AdamMovies/static/css/main.ce9a18af.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/AdamMovies/static/js/2.3d37fe97.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e8bfd4120d598bf26ab9",
-    "url": "/AdamMovies/static/js/main.b43f36e4.chunk.js"
+    "revision": "019bd50be0dc081113a6",
+    "url": "/AdamMovies/static/js/main.b159f246.chunk.js"
   },
   {
     "revision": "82b5f9a1ca00213602d9",
